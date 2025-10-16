1b24f8278d001ee9dd0040ec831451da8f10104d
Peter Brunner <peter@lugoues.net>
2025-10-15 23:08:44 -0400

    add persistence feature

---
+++ test/persistence/persitence_single_link.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+source dev-container-features-test-lib
+
+# from scenario
+source="/mnt/persistence_data"
+home="/home/vscode"
+
+check "source exists" test -d "${source}"
+check "target symlink" test -L "${home}/.config/tests/nested/deep"
+check "write test file to new linked dir" touch "${home}/.config/tests/nested/deep/test_file"
+check "ensure it exists in target" test -f "${source}/home_vscode_.config_tests_nested_deep/test_file"
+check "ensure user owns target" bash -xc "[[ \$(stat -c '%U' ${source}/home_vscode_.config_tests_nested_deep/test_file) == vscode ]]"
+
+reportResults
