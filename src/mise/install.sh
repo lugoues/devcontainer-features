2614f3e18df1a117203aed6ac0b7346dc3b70016
Peter Brunner <peter@lugoues.net>
2026-06-20 22:31:12 +0000

    bump gh-release

--- src/mise/install.sh
+++ src/mise/install.sh
@@ -15,7 +15,7 @@ ensure_nanolayer nanolayer_location "v0.5.6"
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-extra/features/gh-release:1.0.25" \
+    "ghcr.io/devcontainers-extra/features/gh-release:1.0.26" \
     --option repo="jdx/mise" \
     --option binaryNames="mise" \
     --option version="$VERSION" \
